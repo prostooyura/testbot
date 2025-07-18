a3435ee8f1fbe875f1e2014a40626e2e56062928
prostooyura <yurasherbatov2003@gmail.com>
2025-07-18 19:19:56 +0700

    first

---
+++ run.py
@@ -0,0 +1,21 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+from app.handlers import router
+
+
+async def main():
+    load_dotenv()
+    bot = Bot(token = os.getenv('TOKEN'))
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        print('Бот запущен')
+        asyncio.run(main())
+    except:
+        print('Бот выключен')
